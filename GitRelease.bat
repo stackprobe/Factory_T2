16e425f7398c56e039a3dd42f2e6c0c2ddb8252a
stackprobe <stackprobes@gmail.com>
2023-04-23 02:48:49 +0900

    Voyager 1 is 23894420539.810 kilometers away from the Sun.

--- GitRelease.bat
+++ GitRelease.bat
@@ -4,5 +4,5 @@ CALL mkt
 
 Petra\GitRelease.exe
 
-C:\apps\GitHubRepositoryFilter\GitHubRepositoryFilter.exe GIT-HUB-REPO-UNSAFE-MOD C:\home\GitHub
+C:\apps\GitHubRepositoryFilter\GitHubRepositoryFilter.exe C:\home\GitHub\Factory_T2
 C:\apps\GitCommit\GitCommit.exe v1s C:\home\GitHub\Factory_T2
